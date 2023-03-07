e372365f86274ea748def3a2ddcb868481d787d7
Grace Joh <graceehjoh@gmail.com>
2023-03-07 11:09:05 -0600

    test: Add test for Vendor #potential_revenue

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -6,7 +6,16 @@ RSpec.describe Vendor do
                         price: '$0.75' })
     @item2 = Item.new({ name: 'Tomato',
                         price: '$0.50' })
+    @item3 = Item.new({ name: 'Peach-Raspberry Nice Cream',
+                        price: '$5.30' })
+    @item4 = Item.new({ name: 'Banana Nice Cream',
+                        price: '$4.25' })
     @vendor = Vendor.new('Rocky Mountain Fresh')
+    @vendor2 = Vendor.new('Ba-Nom-a-Nom')
+    @vendor2.stock(@item3, 25)
+    @vendor2.stock(@item4, 50)
+    @vendor3 = Vendor.new('Palisade Peach Shack')
+    @vendor3.stock(@item1, 65)
   end
 
   describe '#initialize' do
@@ -39,6 +48,17 @@ RSpec.describe Vendor do
 
       expect(@vendor.inventory).to eq({ @item1 => 55 })
       expect(@vendor.check_stock(@item1)).to eq(55)
+    end
+  end
+
+  describe '#potential_revenue' do
+    it 'can calculate its revenue if all of its stock was sold' do
+      @vendor.stock(@item1, 35)
+      @vendor.stock(@item2, 7)
+
+      expect(@vendor.potential_revenue).to eq(29.75)
+      expect(@vendor2.potential_revenue).to eq(345.00)
+      expect(@vendor3.potential_revenue).to eq(48.75)
     end
   end
 end
