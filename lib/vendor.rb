b89053e60b408a852cadb6bca663b481ab69d92d
Grace Joh <graceehjoh@gmail.com>
2023-03-07 10:36:45 -0600

    feat: Add Vendor #check_stock and #stock

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -5,4 +5,14 @@ class Vendor
     @name = name
     @inventory = Hash.new(0)
   end
+
+  def check_stock(item)
+    return 0 unless @inventory.include?(item)
+
+    @inventory[item]
+  end
+
+  def stock(item, amount)
+    @inventory[item] += amount
+  end
 end
