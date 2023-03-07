f6f9dae3cf910cd4f1a61f7387d257bb0b3d92e7
Grace Joh <graceehjoh@gmail.com>
2023-03-07 10:36:20 -0600

    test: Add tests for Vendor #check_stock and #stock

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -15,4 +15,30 @@ RSpec.describe Vendor do
       expect(@vendor.name).to eq('Rocky Mountain Fresh')
     end
   end
+
+  describe '#check_stock and #stock' do
+    it 'keeps track of its inventory' do
+      expect(@vendor.inventory).to eq({})
+      expect(@vendor.check_stock(@item1)).to eq(0)
+      expect(@vendor.check_stock(@item2)).to eq(0)
+
+      @vendor.stock(@item1, 30)
+
+      expect(@vendor.inventory).to eq({ @item1 => 30 })
+      expect(@vendor.check_stock(@item1)).to eq(30)
+
+      @vendor.stock(@item2, 12)
+
+      expect(@vendor.inventory).to eq({ @item1 => 30, @item2 => 12 })
+      expect(@vendor.check_stock(@item2)).to eq(12)
+    end
+
+    it 'can add to an item already in stock' do
+      @vendor.stock(@item1, 30)
+      @vendor.stock(@item1, 25)
+
+      expect(@vendor.inventory).to eq({ @item1 => 55 })
+      expect(@vendor.check_stock(@item1)).to eq(55)
+    end
+  end
 end
