90a18e492e3ce1b4370bae74db4ee9ce7c4866cc
Grace Joh <graceehjoh@gmail.com>
2023-03-07 11:30:46 -0600

    feat: Add Vendor #sorted_item_list

--- lib/market.rb
+++ lib/market.rb
@@ -17,4 +17,9 @@ class Market
   def vendors_that_sell(item)
     @vendors.select { |vendor| vendor.inventory.include?(item) }
   end
+
+  def sorted_item_list
+    all_items = @vendors.map { |vendor| vendor.inventory.keys.map(&:name) }
+    all_items.flatten.uniq.sort
+  end
 end
