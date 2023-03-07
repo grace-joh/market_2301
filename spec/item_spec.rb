33e6742506a1fa0f4ee58470738f3c339c5f49e5
Grace Joh <graceehjoh@gmail.com>
2023-03-07 10:10:02 -0600

    test: Add test for Item #initialize

---
+++ spec/item_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+RSpec.describe Item do
+  before(:each) do
+    @item1 = Item.new({ name: 'Peach',
+                        price: '$0.75' })
+    @item2 = Item.new({ name: 'Tomato',
+                        price: '$0.50' })
+  end
+
+  describe '#initialize' do
+    it 'exists and has attributes' do
+      expect(@item1).to be_a Item
+      expect(@item1.name).to eq('Peach')
+      expect(@item1.price).to eq(0.75)
+    end
+
+    it 'can initialize more than one item' do
+      expect(@item2).to be_a Item
+      expect(@item2.name).to eq('Tomato')
+      expect(@item2.price).to eq(0.5)
+    end
+  end
+end
